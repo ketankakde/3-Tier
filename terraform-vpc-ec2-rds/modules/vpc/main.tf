7f7598bf39b8106283651b9267b69a558496a763
Ketan Kakde <ketankakde@outlook.com>
2026-06-05 18:53:09 +0530

    Add public and private subnets with routing

--- terraform-vpc-ec2-rds/modules/vpc/main.tf
+++ terraform-vpc-ec2-rds/modules/vpc/main.tf
@@ -5,3 +5,41 @@ resource "aws_vpc" "main" {
     Name = "demo-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "pub-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr
+
+  tags = {
+    Name = "pri-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
