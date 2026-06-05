c4592626a7df7289597d186c37f118a4f3ae5552
Ketan Kakde <ketankakde@outlook.com>
2026-06-05 20:01:06 +0530

    Add EC2 instance and security group resources

---
+++ terraform-vpc-ec2-rds/modules/ec2/main.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-091138d0f0d41ff90"
+  instance_type = "t3.micro"
+
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data = file("${path.module}/install_docker.sh")
+
+  tags = {
+    Name = "docker-ec2"
+  }
+}
