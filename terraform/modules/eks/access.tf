c3bf1df9240701d8adcf7b063e96b9d69579ec82
Ketan Kakde <ketankakde@outlook.com>
2026-06-16 12:39:02 +0530

    Comment out GitHub Actions EKS access entry
    Comment out the aws_eks_access_entry resource for GitHub Actions.

--- terraform/modules/eks/access.tf
+++ terraform/modules/eks/access.tf
@@ -1,8 +1,8 @@
-resource "aws_eks_access_entry" "github_actions" {
-  cluster_name  = aws_eks_cluster.eks.name
-  principal_arn = "arn:aws:iam::160900807677:role/role-for-github-actions"
-  type          = "STANDARD"
-}
+# resource "aws_eks_access_entry" "github_actions" {
+#  cluster_name  = aws_eks_cluster.eks.name
+#  principal_arn = "arn:aws:iam::160900807677:role/role-for-github-actions"
+#  type          = "STANDARD"
+#}
 
 resource "aws_eks_access_policy_association" "github_admin" {
   cluster_name  = aws_eks_cluster.eks.name
